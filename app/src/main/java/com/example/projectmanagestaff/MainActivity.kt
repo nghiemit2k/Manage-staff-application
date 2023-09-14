37300bd179df09222ffcac554a795861c7a74688
nghiem <nghiem066@gmail.com>
2023-09-14 10:58:19 +0700

    update function in my project

---
+++ app/src/main/java/com/example/projectmanagestaff/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.projectmanagestaff
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
