a25d9c677f1ba33a90074fac9a44afe379b199d7
nghiem <nghiem066@gmail.com>
2023-09-16 14:29:42 +0700

    update LoginFragment, ProfileFragment

--- app/src/main/java/com/example/projectmanagestaff/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/projectmanagestaff/ui/login/LoginFragment.kt
@@ -3,58 +3,69 @@ package com.example.projectmanagestaff.ui.login
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.SavedStateHandle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.projectmanagestaff.MyApplication
 import com.example.projectmanagestaff.R
+import com.example.projectmanagestaff.databinding.FragmentLoginBinding
+import com.example.projectmanagestaff.ui.afterTextChanged
+import com.example.projectmanagestaff.ui.viewmodel.ProfileViewModel
+import com.example.projectmanagestaff.ui.viewmodel.ProfileViewModelFactory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var binding: FragmentLoginBinding
+    private val viewModel: ProfileViewModel by activityViewModels {
+        val repository = (requireActivity().application as MyApplication).repository
+        ProfileViewModelFactory(repository)
+    }
+    private lateinit var navController: NavController
+    private lateinit var saveStateHandle: SavedStateHandle
+    private var isButtonLoginClicked = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        navController = findNavController()
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        binding = FragmentLoginBinding.inflate(inflater,container,false)
+        setUpAction()
+        setUpObserve()
+        return binding.root
+    }
+    private fun setUpAction() {
+        binding.btnLogin.setOnClickListener {
+
+        }
+        binding.btnCancel.setOnClickListener{
+            saveStateHandle[LOGIN_SUCCESSFUL] = false
+            navController.popBackStack()
+        }
+        binding.editLoginEmail.afterTextChanged {
+            val email = binding.editLoginEmail.text.toString()
+            val password = binding.editLoginPassword.text.toString()
+            viewModel.login(email,password)
+
+        }
+        binding.editLoginPassword.afterTextChanged {
+            val email = binding.editLoginEmail.text.toString()
+            val password = binding.editLoginPassword.text.toString()
+            viewModel.login(email,password)
+
+        }
+    }
+    private fun  setUpObserve() {
+
+    }
+    companion object {
+        const val LOGIN_SUCCESSFUL="LOGIN_SUCCESSFUL"
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
--- app/src/main/java/com/example/projectmanagestaff/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/projectmanagestaff/ui/profile/ProfileFragment.kt
@@ -5,56 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.projectmanagestaff.MyApplication
 import com.example.projectmanagestaff.R
+import com.example.projectmanagestaff.databinding.FragmentProfileBinding
+import com.example.projectmanagestaff.ui.home.HomeFragmentDirections
+import com.example.projectmanagestaff.ui.login.LoginFragment
+import com.example.projectmanagestaff.ui.viewmodel.ProfileViewModel
+import com.example.projectmanagestaff.ui.viewmodel.ProfileViewModelFactory
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentProfileBinding
+    private val viewModel: ProfileViewModel by activityViewModels {
+        val repository = (requireActivity().application as MyApplication).repository
+        ProfileViewModelFactory(repository)
     }
 
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navController = findNavController()
+        val currentBackStackEntry = navController.currentBackStackEntry
+        val savedStateHandle = currentBackStackEntry?.savedStateHandle
+        savedStateHandle?.getLiveData<Boolean>(LoginFragment.LOGIN_SUCCESSFUL)?.observe(currentBackStackEntry) {success ->
+            if(!success) {
+                val action = HomeFragmentDirections.actionGlobalHomeFragment2()
+                navController.navigate(action)
+            }
+
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
